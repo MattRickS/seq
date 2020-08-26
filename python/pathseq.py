39569b42f2f797d1e6b20ca72e5f63ceb4241a86
MattRickS <mattalexshaw@gmail.com>
2020-08-26 19:51:58 +0100

    strict padding option

--- python/pathseq.py
+++ python/pathseq.py
@@ -8,18 +8,18 @@ import frameseq
 
 class Sequence(object):
     def __init__(self, prefix, padding, suffix, expression):
-        self._prefix = prefix
+        self.prefix = prefix
         self.padding = padding
-        self._suffix = suffix
+        self.suffix = suffix
         self.frames = expression
 
     def __repr__(self):
         return "{}{}{}({})".format(
-            self._prefix, self.format_padding(), self._suffix, self.frames
+            self.prefix, self.format_padding(), self.suffix, self.frames
         )
 
     def __str__(self):
-        return self._prefix + self.format_padding() + self._suffix
+        return "{}{}{}".format(self.prefix + self.format_padding() + self.suffix)
 
     def __iter__(self):
         return (self[i] for i in self.frames)
@@ -28,14 +28,16 @@ class Sequence(object):
         return len(self.frames)
 
     def __getitem__(self, frame):
-        return "{}{:0{pad}d}{}".format(self._prefix, frame, self._suffix, pad=self.padding)
+        return "{}{:0{pad}d}{}".format(
+            self.prefix, frame, self.suffix, pad=self.padding
+        )
 
     def format_padding(self):
         return "#" * self.padding
 
 
 def split_padding(pattern):
-    match = re.search("#+|%(\d+)d", pattern)
+    match = re.search(r"#+|%(\d+)d", pattern)
     if not match:
         return ("", 0, "")
 
@@ -45,13 +47,16 @@ def split_padding(pattern):
     return pattern[:start], padding, pattern[end:]
 
 
-def find_sequence_by_pattern(pattern):
+def find_sequence_by_pattern(pattern, strict_padding=False):
     prefix, padding, suffix = split_padding(pattern)
     if padding == 0:
         return None
 
+    # Padding is a minimum requirement
     glob_pattern = "{}*{}".format(prefix, suffix)
-    re_pattern = "{}(\d{{{}}}){}".format(prefix, padding, suffix)
+    re_pattern = r"{}(\d{{{}{}}}){}".format(
+        prefix, padding, "" if strict_padding else ",", suffix
+    )
     frames = []
     for path in glob.iglob(glob_pattern):
         match = re.match(re_pattern, path)
@@ -70,18 +75,24 @@ def find_sequences_in_directory(directory):
     mapping = collections.defaultdict(list)
 
     for filename in os.listdir(directory):
-        segments = re.split("(\d+)", os.path.join(filename))
-        mapping[tuple(segments[::2])].append(segments[1::2])
+        match = re.search(r"(\d+)(?:\D+)?$", filename)
+        if match is None:
+            continue
+
+        frame_string = match.group(1)
+        start, end = match.span(1)
+        prefix = filename[:start]
+        suffix = filename[end:]
+        mapping[(prefix, suffix)].append(frame_string)
 
     sequences = []
-    for (prefix, suffix), values in mapping.items():
-        if len(values[0]) > 1:
-            print("Multi-numbered files not currently supported")
-            continue
-        else:
-            padding = len(values[0][0])
-            expression = frameseq.frames_to_expressions([int(val[0]) for val in values])
-
-        sequences.append(Sequence(os.path.join(directory, prefix), padding, suffix, expression))
+    for (prefix, suffix), frame_strings in mapping.items():
+        lengths = {len(f) for f in frame_strings}
+        padding = max(lengths) if len(lengths) == 1 else 1
+        frames = sorted(map(int, frame_strings))
+        expression = frameseq.frames_to_expressions(frames)
+        sequences.append(
+            Sequence(os.path.join(directory, prefix), padding, suffix, expression)
+        )
 
     return sequences
